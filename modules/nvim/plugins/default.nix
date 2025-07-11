122046c436a249a6be547c8b6c17f5ef2408a4e2
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-11 15:50:24 +0900

    enabled cmp

--- modules/nvim/plugins/default.nix
+++ modules/nvim/plugins/default.nix
@@ -1,6 +1,9 @@
 {
+  lz-n = import ./lz-n.nix;
   treesitter = import ./treesitter.nix;
   lualine = import ./lualine.nix;
   telescope = import ./telescope.nix;
   deicons = import ./devicons.nix;
+  blink-cmp = import ./blink-cmp.nix;
+  copilot = import ./copilot.nix;
 }
