bfd841d38af3cd3fb1cb49ecfc58c072e6c0c87d
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-13 16:03:57 +0900

    separated file

---
+++ home/5500U.nix
@@ -0,0 +1,18 @@
+{config, pkgs, inputs, ... }:
+{
+  programs.home-manager.enable = true;
+  systemd.user.startServices = true;
+  imports = [
+    ../modules/cli.nix
+    ../modules/nvim/default.nix
+    ../modules/gui.nix
+    ../modules/localization/fcitx5.nix
+    ../modules/theme/default.nix
+    ../modules/wayland/core.nix
+    ../modules/wayland/wlogout/default.nix
+    ../modules/wayland/hypr/default.nix
+    ../modules/wayland/hypr/monitor/hyprland-monitor-mini.nix
+    ../modules/rustdesk.nix
+    ../modules/systemd/nixos-home-manager.nix
+  ];
+}
