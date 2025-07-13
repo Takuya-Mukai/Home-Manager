83133d76c873db157df26d90f6b0bef994a8423b
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-13 16:14:31 +0900

    which-key-ui changed

--- modules/nvim/plugins/which-key.nix
+++ modules/nvim/plugins/which-key.nix
@@ -1,5 +1,26 @@
 {
   programs.nixvim.plugins.which-key = {
     enable = true;
+    settings = {
+      win = {
+        border = "rounded";
+        zindex = 1000;
+        wo.winblend = 20;
+        height = {
+          min = 10;
+          max = 20;
+        };
+        width = 0.4;
+        row = 1;
+        col.__raw = ''vim.o.columns - math.floor(vim.o.columns * 0.4) - 2'';
+      };
+      layout = {
+        width = {
+          min = 20;
+          max = 0.4;
+        };
+        align = "left";
+      };
+    };
   };
 }
